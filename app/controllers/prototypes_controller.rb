debd2ee0c3d5cf9663ce10be7fbdd902374163f9
watsontaka <139229993+watsontaka@users.noreply.github.com>
2023-09-05 23:35:21 +0900

    commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -25,7 +25,7 @@ class PrototypesController < ApplicationController
     prototype = Prototype.find(params[:id])
     prototype.update(prototype_params)
     if prototype.save
-      redirect_to user_path(@prototype.user_id)
+      redirect_to user_path(prototype.user_id)
     else
       render :edit, status: :unprocessable_entity
     end
