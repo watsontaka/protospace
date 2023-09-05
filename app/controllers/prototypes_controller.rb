e303fd3108823716a3d1f28a1f741a7a3a5b485e
watsontaka <139229993+watsontaka@users.noreply.github.com>
2023-09-06 00:52:48 +0900

    commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:new,:edit,:delete]
   before_action :set_prototype,      only: [:edit, :show]
-  before_action :move_to_index,      only: [:edit,:update]
+  before_action :move_to_index,      except: [:edit,:update]
 
 
   def index
@@ -56,9 +56,9 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? == current_user.id
-      redirect_to root_path
-    end
+      @prototype = Prototype.find(params[:id])
+      @user = @prototype.user
+      redirect_to root_path unless @user == current_user
   end
 
 end
